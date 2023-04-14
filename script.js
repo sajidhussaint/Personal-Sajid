36cbe1752e30fe8a7379963204fe8bb778794129
SAJID HUSSAIN <53827080+sajidhussaint@users.noreply.github.com>
2023-04-14 09:53:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+// function validate() {
+//     var name = document.forms["form"]["name"];
+//     var email = document.forms["form"]["email"];
+//     var subject = document.forms["form"]["subject"];
+//     var message = document.forms["form"]["message"];
+
+//     if (name.value == "") {
+//         window.alert("Please enter your name.");
+//         name.focus();
+//         return false;
+//     }
+
+//     if (email.value == "") {
+//         window.alert(
+//             "Please enter a valid e-mail address.");
+//         email.focus();
+//         return false;
+//     }
+//     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(form.email.value)) {
+//     } else {
+//         window.alert("You have entered an invalid email address!")
+
+//         return false;
+
+//     }
+//     if (subject.value == "") {
+//         window.alert(
+//             "Please enter subject.");
+//         subject.focus();
+//         return false;
+//     }
+
+//     if (message.value == "") {
+//         window.alert("Please enter your message");
+//         message.focus();
+//         return false;
+//     }
+
+
+
+// }
+
